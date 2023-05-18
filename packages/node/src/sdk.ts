4e4d617794d903aa82e14eb49e1a429a65800d71
Gabe Jackson <17556281+gj@users.noreply.github.com>
2023-05-18 12:40:50 -0400

    Basic test

--- packages/node/src/sdk.ts
+++ packages/node/src/sdk.ts
@@ -1,9 +1,23 @@
 import { version } from '../package.json';
 import { OSO_URL } from './consts';
-import { Oso } from 'oso-cloud';
+import { Fact, Instance, Oso } from 'oso-cloud';
 
 export class OsoSdk extends Oso {
   constructor(apiKey: string, userAgent?: string) {
     super(OSO_URL, apiKey, userAgent || `OsoSdk/${String(version)}`);
+  }
+
+  async authorize(
+    actor: Instance,
+    action: string,
+    resource: Instance,
+    contextFacts?: Fact[] | undefined
+  ): Promise<boolean> {
+    try {
+      return await super.authorize(actor, action, resource, contextFacts);
+    } catch (e) {
+      console.error('GOT', e);
+      return true;
+    }
   }
 }
--- packages/node/test/index.test.ts
+++ packages/node/test/index.test.ts
@@ -25,3 +25,58 @@ describe('test init Oso handle', () => {
     expect(globalOso()).not.toEqual(instanceOso);
   });
 });
+
+describe('can still serve responses when Oso Cloud is unreachable', () => {
+  expect.assertions(3);
+
+  let oso: OsoSdk;
+  let mockReq: jest.SpyInstance;
+
+  // OsoSdk > Oso > Api > _req
+  beforeEach(() => {
+    oso = init({ apiKey: 'YOUR_API_KEY', shared: false }, OsoSdk);
+    const api = oso.api;
+
+    mockReq = jest.spyOn(api, '_req');
+  });
+
+  test('basic test', async () => {
+    const errorMessage = '500 buddy';
+    mockReq
+      .mockImplementationOnce(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        (_path, _method, _params, _body) => Promise.resolve({ allowed: true })
+      )
+      .mockImplementation(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        (_path, _method, _params, _body) =>
+          Promise.reject(new Error(errorMessage))
+      );
+
+    const beforeTheOutage = oso.authorize('cache', 'rules', 'everything');
+    await expect(beforeTheOutage).resolves.toBe(true);
+
+    // Oso Cloud outage begins...
+
+    // Sanity check that mocks are working as expected.
+    const cacheMiss = oso.authorize('cache', 'is', 'beans');
+    await expect(cacheMiss).rejects.toThrowError(errorMessage);
+
+    const cacheHit = oso.authorize('cache', 'rules', 'everything');
+    await expect(cacheHit).resolves.toBe(true);
+
+    // expect(mockReq).toHaveBeenCalledWith(
+    //   '/authorize',
+    //   'POST',
+    //   {},
+    //   {
+    //     action: 'rules',
+    //     actor_id: 'cache',
+    //     actor_type: 'String',
+    //     context_facts: [],
+    //     resource_id: 'everything',
+    //     resource_type: 'String',
+    //   }
+    // );
+  });
+});
